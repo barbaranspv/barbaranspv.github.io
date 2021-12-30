75b0fa37bfcd1faf88e66ecbe671ad51941eae39
Barbara <barbara.vieira99@gmail.com>
2021-12-30 00:57:53 +0000

    timeline and projects

---
+++ index.js
@@ -0,0 +1,55 @@
+var items = document.querySelectorAll(".timeline li");
+
+var cards = document.querySelectorAll(".card");
+
+var elements;
+  var windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+  }
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight+150 || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      if(!items[i].classList.contains("in-view")){
+        items[i].classList.add("in-view");
+      }
+    } else if(items[i].classList.contains("in-view")) {
+        items[i].classList.remove("in-view");
+        
+    }
+}for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    var positionFromTop = elements[i].getBoundingClientRect().top;
+
+    if (isElementInViewport(elements[i])) {
+      element.classList.add('fade-in-element');
+      element.classList.remove('hidden');
+    }
+  }
+}
+
+$("a[href='#top']").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+  
+init();
+
+document.body.addEventListener('scroll', callbackFunc);
+window.addEventListener("load", callbackFunc);
+window.addEventListener('resize', init);
+
+//window.addEventListener("scroll", callbackFunc);
